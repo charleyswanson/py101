0f2bfab9698ce65ebf697983fd4ea81c75cbb050
charleyswanson <charleyer@gmail.com>
2025-02-26 14:04:50 +0000

    Easy 2 : exercises 7 and 8

--- easy2/ex7_2.py
+++ easy2/ex7_2.py
@@ -3,13 +3,14 @@
 # In this exercise, you will write an xor function that takes two arguments, and returns True if exactly one of its arguments is truthy, False otherwise.
 
 def xor(val1, val2):
-    if val1 and not val2:
+    if (val1 and not val2) or (val2 and not val1):
         return True
-    elif val2 and not val1:
-        return True
-    else:
-        return False
+    return False
 
+# print(xor(5, 0))
+# print(xor(False, True))
+# print(xor(1, 1))
+# print(xor(True, True))
 
 print(xor(5, 0) == True)
 print(xor(False, True) == True)
