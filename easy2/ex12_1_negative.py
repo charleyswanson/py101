32bc854823d93a26b5b47aec40ebf1f33fbfb3e7
charleyswanson <charleyer@gmail.com>
2025-02-28 00:39:04 +0000

    Easy 2 : exercises 11 and 12

---
+++ easy2/ex12_1_negative.py
@@ -0,0 +1,8 @@
+# Write a function that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the argument is a negative number, return it as-is.
+
+def negative(number):
+    return abs(number) * -1
+
+print(negative(5) == -5)      # True
+print(negative(-3) == -3)     # True
+print(negative(0) == 0)       # True
