d55f678ad6d8a81db73c1038ce8b7f105680491a
charleyswanson <charleyer@gmail.com>
2025-03-03 00:14:06 +0000

    Easy 3 : ex 4, 5 and 6

---
+++ easy3/ex4_1_strings.py
@@ -0,0 +1,17 @@
+# Write a function that takes one argument, a positive integer, and returns a string of alternating '1's and '0's, always starting with a '1'. The length of the string should match the given integer.
+
+def stringy(length):
+    output = ''
+    for _ in range(length):
+        if output == '' or output[-1] == '0':
+            number = '1'
+        else:
+            number = '0'
+        output += number
+    
+    return output
+
+print(stringy(6) == "101010")           # True
+print(stringy(9) == "101010101")        # True
+print(stringy(4) == "1010")             # True
+print(stringy(7) == "1010101")          # True
