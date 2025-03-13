9f384abc74d2d6ca7a2e47511567850245fe129c
charleyswanson <charleyer@gmail.com>
2025-03-13 20:04:57 +0000

    Lesson 2 - loan calculator: clear screen, messages cleaned up, while loop improved

--- lesson_2/calculator_loan1.py
+++ lesson_2/calculator_loan1.py
@@ -1,4 +1,6 @@
 import json
+import os
+import platform
 
 # Load the messages from the JSON file
 with open('calculator_loan_messages.json', 'r') as file:
@@ -12,7 +14,9 @@ def invalid_int(number):
         int(number)
     except ValueError:
         return True
-    
+    if int(number) <= 0:
+        return True
+
     return False
 
 def invalid_float(number):
@@ -20,47 +24,72 @@ def invalid_float(number):
         float(number)
     except ValueError:
         return True
-    
+    if float(number) < 0:
+        return True
+
     return False
 
-prompt(MESSAGES['msg_welcome'])
+def clear_screen():
+    if platform.system() == "Windows":
+        os.system('cls')
+    else:
+        os.system('clear')
 
-# get loan amount, verify input, convert to integer
-print()
-prompt(MESSAGES['msg_loan_amount'])
-print('$', end='')
-loan_amount = input()
-
-while invalid_int(loan_amount):
-    prompt(MESSAGES['msg_invalid_int'])
+while True:
+    clear_screen()
+    prompt(MESSAGES['msg_welcome'])
+    prompt(MESSAGES['msg_inputs'])
+    # get loan amount, verify input, convert to integer
+    print()
+    prompt(MESSAGES['msg_loan_amount'])
+    print('$', end='')
     loan_amount = input()
 
-loan_amount = int(loan_amount)
+    while invalid_int(loan_amount):
+        prompt(MESSAGES['msg_invalid_int'])
+        loan_amount = input()
 
-# get APR, verify input, convert to montly APR
-print()
-prompt(MESSAGES['msg_apr'])
-apr = input()
+    loan_amount = int(loan_amount)
 
-while invalid_float(apr):
-    prompt(MESSAGES['msg_invalid_float'])
+    # get APR, verify input, convert to montly APR
+    print()
+    prompt(MESSAGES['msg_apr'])
     apr = input()
 
-monthly_apr = (float(apr) / 12) / 100
+    while invalid_float(apr):
+        prompt(MESSAGES['msg_invalid_float'])
+        apr = input()
 
-# get loan duration, error check, convert to integer
-print()
-prompt(MESSAGES['msg_duration'])
-duration = input()
+    monthly_apr = (float(apr) / 12) / 100
 
-while invalid_int(duration):
-    prompt(MESSAGES['msg_invalid_int'])
+    # get loan duration, error check, convert to integer
+    print()
+    prompt(MESSAGES['msg_duration'])
     duration = input()
 
-duration = int(duration)
+    while invalid_int(duration):
+        prompt(MESSAGES['msg_invalid_int'])
+        duration = input()
 
-# calculate monthly payment
-monthly_payment = loan_amount * (monthly_apr / (1 - (1 + monthly_apr) ** (-duration)))
+    duration = int(duration)
 
-print()
-print(f'Your monthly payment will be ${monthly_payment:.2f}')
+    # calculate monthly payment
+    if apr == '0':
+        monthly_payment = loan_amount / duration
+    else:
+        denominator = (1 - (1 + monthly_apr) ** (-duration))
+        monthly_payment = loan_amount * (monthly_apr / denominator)
+
+    print()
+    print(f'--> Your monthly payment will be ${monthly_payment:.2f}')
+
+    print()
+    prompt(MESSAGES['msg_calculate_again'])
+    
+    again = input()
+    if not again or again[0].lower() != 'y':
+        prompt(MESSAGES['msg_end_thanks'])
+        break
+    # if again[0].lower() != 'y':
+    #     prompt(MESSAGES['msg_end_thanks'])
+    #     break
